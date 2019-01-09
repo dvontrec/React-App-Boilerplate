c5c2244a6a9c02b4be64004f9b564bd56889712e
Dvontre Coleman <dvontrec@gmail.com>
2019-01-09 10:46:02 -0500

    Add cache hashing and build cleaning for dev build

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,24 @@
 // File needed to setup webpack for project
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+// list of dependencies should be altered as more are added
+const VENDER_LIBS = ['react', 'react-dom'];
 
 // Create a config object
 const config = {
   // Requires 2 min properties
   // Entry property (The root of the application/ first file to execute)
-  entry: './src/index.js',
+  entry: {
+    // set an entry for pur bundle and vender bundle for chink hashing
+    bundle: './src/index.js',
+    vender: VENDER_LIBS
+  },
   // Output property (Where to save all combined modules to)
   output: {
     //path directory where file is saved (saves to absolute current path)
     path: path.resolve(__dirname, 'build'),
-    // name of the file
-    filename: 'bundle.js'
+    // name of the file with a hash for cache
+    filename: '[name].[chunkhash].js'
   },
   // Allows webpack to use babel loaders/rules
   module: {
