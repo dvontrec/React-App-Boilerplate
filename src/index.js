ee9814fd0fc6148f4f2d865332280992ee0d147d
Dvontre Coleman <dvontrec@gmail.com>
2019-01-09 09:27:53 -0500

    Add basic react stuff

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+// Basics for react app rendering
+import React from 'react';
+import ReactDom from 'react-dom';
+
+// App setup basics
+const App = () => {
+  return <div>Hello React!</div>;
+};
+
+// Renders the App element in the document element with the root ID
+ReactDOM.render(<App />, document.getElementById('root'));
